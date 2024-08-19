d60ac81ec4bab9c53b1667a6d005ce9ae752b648
kaosfactor <primexfifty@gmail.com>
2024-08-19 01:30:45 -0400

    Add files via upload

---
+++ js/StaticRadarX.js
@@ -0,0 +1,256 @@
+function guessZipCode(){
+
+  return;
+
+  var zipCodeElement = getElement("zip-code-text");
+  if(zipCodeElement.value != ""){
+    return;
+  }
+
+  fetch(`https://api.wunderground.com/api/${CONFIG.secrets.wundergroundAPIKey}/geolookup/q/autoip.json`)
+    .then(function(response) {
+      //check for error
+      if (response.status !== 200) {
+        console.log("zip code request error");
+        return;
+      }
+
+      response.json().then(function(data) {
+        if(zipCodeElement.value == ""){
+          zipCodeElement.value = data.location.zip;
+        }
+
+      });
+    })
+}
+
+function fetchAlerts(){
+  var alertCrawl = "";
+  fetch(`https://api.weather.gov/alerts/active?point=${latitude},${longitude}`)
+    .then(function(response) {
+        if (response.status !== 200) {
+            console.warn("Alerts Error, no alerts will be shown");
+        }
+      response.json().then(function(data) {
+        if (data.features == undefined){
+          fetchForecast();
+          return;
+        }
+        if (data.features.length == 1) {
+          alerts[0] = data.features[0].properties.event + '<br>' + data.features[0].properties.description.replace("..."," ").replace(/\*/g, "")
+          for(var i = 0; i < data.features.length; i++){
+            /* Take the most important alert message and set it as crawl text
+            This will supply more information i.e. tornado warning coverage */
+            alertCrawl = alertCrawl + " " + data.features[i].properties.description.replace("...", " ");
+          }
+        }
+        else {
+          for(var i = 0; i < data.features.length; i++){
+            /* Take the most important alert message and set it as crawl text
+            This will supply more information i.e. tornado warning coverage */
+            alertCrawl = alertCrawl + " " + data.features[i].properties.description.replace("...", " ");
+
+            alerts[i] = data.features[i].properties.event
+          }
+        }
+        if(alertCrawl != ""){
+          CONFIG.crawl = alertCrawl;
+        }
+        alertsActive = alerts.length > 0;
+        fetchForecast();
+      });
+    })
+}
+
+function fetchForecast(){
+  fetch(`https://api.weather.com/v1/geocode/${latitude}/${longitude}/forecast/daily/10day.json?language=${CONFIG.language}&units=${CONFIG.units}&apiKey=${CONFIG.secrets.twcAPIKey}`)
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log('forecast request error');
+        return;
+      }
+      response.json().then(function(data) {
+        let forecasts = data.forecasts
+        // narratives
+        isDay = forecasts[0].day; // If the API spits out a day forecast, use the day timings
+        let ns = []
+        ns.push(forecasts[0].day || forecasts[0].night); // there must be a day forecast so if the API doesn't provide one, just make it the night one. It won't show anyway.
+        ns.push(forecasts[0].night);
+        ns.push(forecasts[1].day);
+        ns.push(forecasts[1].night);
+        for (let i = 0; i <= 3; i++) {
+          let n = ns[i]
+          forecastTemp[i] = n.temp
+          forecastIcon[i] = n.icon_code
+          forecastNarrative[i] = n.narrative
+          forecastPrecip[i] = `${n.pop}% Chance<br/> of ${n.precip_type.charAt(0).toUpperCase() + n.precip_type.substr(1).toLowerCase()}`
+        }
+
+        fetchRadarImages();
+      })
+    })
+}
+
+function fetchCurrentWeather(){
+
+  //Let's check what we're dealing with
+  let location = "";
+  console.log(CONFIG.locationMode)
+  if(CONFIG.locationMode=="POSTAL") {location=`postalKey=${zipCode}:${CONFIG.countryCode}`}
+  else if (CONFIG.locationMode=="AIRPORT") {
+    //Determine whether this is an IATA or ICAO code
+    let airportCodeLength=airportCode.length;
+    if(airportCodeLength==3){location=`iataCode=${airportCode}`}
+    else if (airportCodeLength==4){location=`icaoCode=${airportCode}`}
+    else {
+      alert("Please enter a valid ICAO or IATA Code")
+      console.error(`Expected Airport Code Lenght to be 3 or 4 but was ${airportCodeLength}`)
+      return;
+    }
+  }
+  else {
+    alert("Please select a location type");
+    console.error("Unknown what to use for location")
+    return;
+  }
+  
+
+  fetch(`https://api.weather.com/v3/location/point?${location}&language=${CONFIG.language}&format=json&apiKey=${CONFIG.secrets.twcAPIKey}`)
+      .then(function (response) {
+          if (response.status == 404) {
+              alert("Location not found!")
+              console.log('conditions request error');
+              return;
+          }
+          if (response.status !== 200) {
+              alert("Something went wrong (check the console)")
+              console.log('conditions request error');
+              return;
+          }
+      response.json().then(function(data) {
+        try {
+          if(CONFIG.locationMode=="AIRPORT"){
+            cityName = data.location.airportName
+            .toUpperCase() //Airport names are long
+            .replace("INTERNATIONAL","INTL.") //If a city name is too long, info bar breaks
+            .replace("AIRPORT","") //This is an attempt to fix it
+            .trim();
+            console.log(cityName);
+          } else {
+            //Shouldn't City Name be the field City Name, not Display Name?
+            cityName = data.location.city.toUpperCase();
+          }
+          latitude = data.location.latitude;
+          longitude = data.location.longitude;
+        } catch (err) {
+          alert('Enter valid ZIP code');
+          console.error(err)
+          getZipCodeFromUser();
+          return;
+        }
+        fetch(`https://api.weather.com/v1/geocode/${latitude}/${longitude}/observations/current.json?language=${CONFIG.language}&units=${CONFIG.units}&apiKey=${CONFIG.secrets.twcAPIKey}`)
+          .then(function(response) {
+            if (response.status !== 200) {
+              console.log("conditions request error");
+              return;
+            }
+            response.json().then(function(data) {
+              // cityName is set in the above fetch call and not this one
+              let unit = data.observation[CONFIG.unitField];
+              currentTemperature = Math.round(unit.temp);
+              currentCondition = data.observation.phrase_32char;
+              windSpeed = `${data.observation.wdir_cardinal} ${unit.wspd} ${CONFIG.units === 'm' ? 'km/h' : 'mph'}`;
+              gusts = unit.gust || 'NONE';
+              feelsLike = unit.feels_like
+              visibility = Math.round(unit.vis)
+              humidity = unit.rh
+              dewPoint = unit.dewpt
+              pressure = unit.altimeter.toPrecision(4);
+              let ptendCode = data.observation.ptend_code
+              pressureTrend = (ptendCode == 1 || ptendCode == 3) ? '▲' : ptendCode == 0 ? '' : '▼'; // if ptendCode == 1 or 3 (rising/rising rapidly) up arrow else its steady then nothing else (falling (rapidly)) down arrow
+              currentIcon = data.observation.icon_code
+              fetchAlerts();
+            });
+          });
+      })
+    });
+
+
+}
+
+function fetchRadarImages(){
+  radarImage = document.createElement("iframe");
+  radarImage.onerror = function () {
+    getElement('radar-container').style.display = 'none';
+  }
+
+
+scheduleTimeline();
+
+      // set up API credentials
+      mapboxgl.accessToken = "pk.eyJ1IjoiYmxhcmsiLCJhIjoiY2plaGZmaGR1MGZ3cTJ3bzZ6OHp5OGZzYyJ9.5dVrsWJk208YPShD-0HLsQ";
+      const twcApiKey = "e1f10a1e78da46f5b10a1e78da96f525";
+
+      // set up a promise for The Weather Company product metadata
+      const timeSlices = fetch(
+        "https://api.weather.com/v3/TileServer/series/productSet/PPAcore?apiKey=" +
+          twcApiKey
+      );
+
+
+
+      // set up map
+      const map = new mapboxgl.Map({
+        container: "map", // container id
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [longitude, latitude], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+
+
+
+      // this function resolves the metadata promise,
+      // extracts the most recent publish time for radar data,
+      // and adds the radar layer to the map
+      const addRadarLayer = () => {
+        timeSlices
+          .then((res) => res.json())
+          .then((res) => {
+            const radarTimeSlices = res.seriesInfo.radar.series;
+            const latestTimeSlice = radarTimeSlices[0].ts;
+
+            // insert the latest time for radar into the source data URL
+            map.addSource("twcRadar", {
+              type: "raster",
+              tiles: [
+                "https://api.weather.com/v3/TileServer/tile/radar?ts=" +
+                  latestTimeSlice +
+                  "&xyz={x}:{y}:{z}&apiKey=" +
+                  twcApiKey,
+              ],
+              tileSize: 256,
+            });
+
+            // place the layer before the "aeroway-line" layer
+            map.addLayer(
+              {
+                id: "radar",
+                type: "raster",
+                source: "twcRadar",
+                paint: {
+                  "raster-opacity": 0.5,
+                },
+              },
+              "aeroway-line"
+            );
+          });
+      };
+
+      map.on("load", () => {
+        addRadarLayer();
+      });
+
+
+}
+
+
