54568fdea98796329f8ff8681196bbd45d6711ff
kaosfactor <primexfifty@gmail.com>
2024-08-27 12:49:29 -0400

    Add files via upload

--- js/GeoRadar2.js
+++ js/GeoRadar2.js
@@ -1,18 +1,11 @@
-
 function guessZipCode(){
-  // Skip geolookup until replaced with TWC (wunderground api dead)
+
   return;
 
   var zipCodeElement = getElement("zip-code-text");
-  // Before filling with auto zip, check and see if
-  // there is already an input
   if(zipCodeElement.value != ""){
     return;
   }
-
-  // always use wunderground API for geolookup
-  // only valid equivalent is GET v3/location/search
-  // TODO: use TWC API GET v3/location/search instead of wunderground geolookup
   fetch(`https://api.wunderground.com/api/${CONFIG.secrets.wundergroundAPIKey}/geolookup/q/autoip.json`)
     .then(function(response) {
       //check for error
@@ -21,8 +14,6 @@ function guessZipCode(){
         return;
       }
       response.json().then(function(data) {
-        // Only fill zip if the user didn't touch
-        // the box while the zip was fetching
         if(zipCodeElement.value == ""){
           zipCodeElement.value = data.location.zip;
         }
@@ -30,89 +21,8 @@ function guessZipCode(){
     })
 }
 
-function fetchAlerts(){
-  var alertCrawl = "";
-  fetch(`https://api.weather.gov/alerts/active?point=${latitude},${longitude}`)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.warn("Alerts Error, no alerts will be shown");
-        }
-      response.json().then(function(data) {
-        if (data.features == undefined){
-          fetchForecast();
-          return;
-        }
-        if (data.features.length == 1) {
-          alerts[0] = data.features[0].properties.event + '<br>' + data.features[0].properties.description.replace("..."," ").replace(/\*/g, "")
-          for(var i = 0; i < data.features.length; i++){
-            /* Take the most important alert message and set it as crawl text
-            This will supply more information i.e. tornado warning coverage */
-            alertCrawl = alertCrawl + " " + data.features[i].properties.description.replace("...", " ");
-          }
-        }
-        else {
-          for(var i = 0; i < data.features.length; i++){
-            /* Take the most important alert message and set it as crawl text
-            This will supply more information i.e. tornado warning coverage */
-            alertCrawl = alertCrawl + " " + data.features[i].properties.description.replace("...", " ");
-
-            alerts[i] = data.features[i].properties.event
-          }
-        }
-        if(alertCrawl != ""){
-          CONFIG.crawl = alertCrawl;
-        }
-        alertsActive = alerts.length > 0;
-        fetchForecast();
-      });
-    })
-}
-
-function fetchForecast(){
-  fetch(`https://api.weather.com/v1/geocode/${latitude}/${longitude}/forecast/daily/10day.json?language=${CONFIG.language}&units=${CONFIG.units}&apiKey=${CONFIG.secrets.twcAPIKey}`)
-    .then(function(response) {
-      if (response.status !== 200) {
-        console.log('forecast request error');
-        return;
-      }
-      response.json().then(function(data) {
-        let forecasts = data.forecasts
-        // narratives
-        isDay = forecasts[0].day; // If the API spits out a day forecast, use the day timings
-        let ns = []
-        ns.push(forecasts[0].day || forecasts[0].night); // there must be a day forecast so if the API doesn't provide one, just make it the night one. It won't show anyway.
-        ns.push(forecasts[0].night);
-        ns.push(forecasts[1].day);
-        ns.push(forecasts[1].night);
-        for (let i = 0; i <= 3; i++) {
-          let n = ns[i]
-          forecastTemp[i] = n.temp
-          forecastIcon[i] = n.icon_code
-          forecastNarrative[i] = n.narrative
-          forecastPrecip[i] = `${n.pop}% Chance<br/> of ${n.precip_type.charAt(0).toUpperCase() + n.precip_type.substr(1).toLowerCase()}`
-        }
-        // 7 day outlook
-        for (var i = 0; i < 7; i++) {
-          let fc = forecasts[i+1]
-          outlookHigh[i] = fc.max_temp
-          outlookLow[i] = fc.min_temp
-          outlookCondition[i] = (fc.day ? fc.day : fc.night).phrase_32char.split(' ').join('<br/>')
-          // thunderstorm doesn't fit in the 7 day outlook boxes
-          // so I multilined it similar to that of the original
-          outlookCondition[i] = outlookCondition[i].replace("Thunderstorm", "Thunder</br>storm");
-          outlookIcon[i] = (fc.day ? fc.day : fc.night).icon_code
-        }
-        fetchRadarImages();
-
-      })
-    })
-}
-
-
 
 function fetchCurrentWeather(){
-
-  //Let's check what we're dealing with
   let location = "";
   console.log(CONFIG.locationMode)
   if(CONFIG.locationMode=="POSTAL") {location=`postalKey=${zipCode}:${CONFIG.countryCode}`}
@@ -128,28 +38,12 @@ function fetchCurrentWeather(){
     }
   }
   else {
-    alert("Please select a location type");
-    console.error("Unknown what to use for location")
-    return;
-  }
-  
 
+  }
   fetch(`https://api.weather.com/v3/location/point?${location}&language=${CONFIG.language}&format=json&apiKey=${CONFIG.secrets.twcAPIKey}`)
       .then(function (response) {
-          if (response.status == 404) {
-              alert("Location not found!")
-              console.log('conditions request error');
-              return;
-          }
-          if (response.status !== 200) {
-              alert("Something went wrong (check the console)")
-              console.log('conditions request error');
-              return;
-          }
       response.json().then(function(data) {
         try {
-          // which LOCALE?!
-          //Not sure about the acuracy of this. Remove this if necessary
           if(CONFIG.locationMode=="AIRPORT"){
             cityName = data.location.airportName
             .toUpperCase() //Airport names are long
@@ -169,83 +63,15 @@ function fetchCurrentWeather(){
           getZipCodeFromUser();
           return;
         }
-        fetch(`https://api.weather.com/v1/geocode/${latitude}/${longitude}/observations/current.json?language=${CONFIG.language}&units=${CONFIG.units}&apiKey=${CONFIG.secrets.twcAPIKey}`)
-          .then(function(response) {
-            if (response.status !== 200) {
-              console.log("conditions request error");
-              return;
-            }
-            response.json().then(function(data) {
-              // cityName is set in the above fetch call and not this one
-              let unit = data.observation[CONFIG.unitField];
-              currentTemperature = Math.round(unit.temp);
-              currentCondition = data.observation.phrase_32char;
-              windSpeed = `${data.observation.wdir_cardinal} ${unit.wspd} ${CONFIG.units === 'm' ? 'km/h' : 'mph'}`;
-              gusts = unit.gust || 'NONE';
-              feelsLike = unit.feels_like
-              visibility = Math.round(unit.vis)
-              humidity = unit.rh
-              dewPoint = unit.dewpt
-              pressure = unit.altimeter.toPrecision(4);
-              let ptendCode = data.observation.ptend_code
-              pressureTrend = (ptendCode == 1 || ptendCode == 3) ? '▲' : ptendCode == 0 ? '' : '▼'; // if ptendCode == 1 or 3 (rising/rising rapidly) up arrow else its steady then nothing else (falling (rapidly)) down arrow
-              currentIcon = data.observation.icon_code
-              fetchAlerts();
-            });
-          });
+              fetchRadarImages();
+
       })
     });
-
-
 }
 
-
-
-
 function fetchRadarImages(){
-  radarImage = document.createElement("iframe");
-  radarImage.onerror = function () {
-    getElement('radar-container').style.display = 'none';
-  }
 
-
-
-  mapSettings = btoa(JSON.stringify({
-    "agenda": {
-      "id": "weather",
-      "center": [longitude, latitude],
-      "location": null,
-      "zoom": 10
-    }
-
-  }));
-  
-  
-  if(alertsActive){
-    zoomedRadarImage = new Image();
-    zoomedRadarImage.onerror = function () {
-      getElement('zoomed-radar-container').style.display = 'none';
-    }
-
-    zoomedRadarImage = document.createElement("iframe");
-    zoomedRadarImage.onerror = function () {
-      getElement('zoomed-radar-container').style.display = 'none';
-    }
-  
-    mapSettings = btoa(JSON.stringify({
-      "agenda": {
-        "id": "weather",
-        "center": [longitude, latitude],
-        "location": null,
-        "zoom": 10
-      }
-    }));
-  }
-
-
-
-
-  scheduleTimeline();
+scheduleTimeline();
   //window.location.href = "https://radar.weather.gov/?settings=v1_"+mapSettings;
 
  
@@ -292,7 +118,7 @@ function getTilePath(server, interval) {
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhcmsiLCJhIjoiY2plaGZmaGR1MGZ3cTJ3bzZ6OHp5OGZzYyJ9.5dVrsWJk208YPShD-0HLsQ';
 const map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
+	style: 'mapbox://styles/mapbox/bright-v9',
 	center: [longitude, latitude],
 	zoom: 9,
 	projection: 'mercator'
